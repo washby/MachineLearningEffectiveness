20e1ebcebb9998be243cc0fdeda02367204bd7b4
Wade Ashby <wade.ashby@hsutx.edu>
2023-03-26 12:32:14 -0500

    First commit of code to build.

---
+++ CanvasSystem/AbstractCanvasObject.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+
+class AbstractCanvasObject:
+    def __init__(self):
+        self._id = None
+        self._end_date = None
+        self._start_date = None
+        self._name = None
+
+    def _set_start_end_dates(self, start_at_string, end_at_string):
+        if start_at_string is not None and start_at_string.strip() != 'None':
+            self._start_date = datetime.fromisoformat(start_at_string.strip())
+        if end_at_string is not None and end_at_string.strip() != 'None':
+            self._end_date = datetime.fromisoformat(end_at_string.strip())
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def end_date(self):
+        return self._end_date
+
+    @property
+    def start_date(self):
+        return self._start_date
+
+    @property
+    def name(self):
+        return self._name
